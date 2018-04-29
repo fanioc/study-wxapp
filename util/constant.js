4e1743bf28f41ff22310140781ae42d732fc2451
卢林杨 <281203035@qq.com>
2018-04-29 10:13:14 +0800

    constant.js及image文件修改
    1，不同主题存放在imag目录的不同文件夹内
    2，添加了path全局变量

--- util/constant.js
+++ util/constant.js
@@ -5,6 +5,12 @@ const URL=
     base:"http://localhost/wx/",
     debug:"http://localhost/a.php"
 }
+//2.关于路径的常量，所有资源都以此为前缀
+const PATH=
+{
+  icon:"/image/subject/"
+}
 module.exports = {
-  URL: URL
+  URL: URL,
+  PATH: PATH
 }
