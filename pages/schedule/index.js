5ca404bbb6768eef758c07edefd15b106914282d
Fanioc <fanioc@163.com>
2018-05-02 00:19:13 +0800

    完善课表功能，修改社区图片

--- pages/schedule/index.js
+++ pages/schedule/index.js
@@ -10,47 +10,85 @@ Page({
         week: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
         date: [23, 24, 25, 26, 27, 28, 29],
         time: ["08:00", "09:00", "10:00", "11:00", "14:10", "15:10", "16:10", "17:10", "19:00", "20:00"]
-      }
-
+      },
+      currentWeek: 8,
     },
-    //测试数据
-    term: {
-      year: 2018,
-      week: 8
-    },
-    sche: [
-      {
-        'className': '高等数学',
-        'timeable': '1|1-2',// 数据格式：星期|第几节-第几节课
-        'week': '1-18|0',// 数据格式：第几周-第几周|所有单双周0 1 2
-        'location': '一号教学楼101'
-      }, {
-        'className': '大学英语',
-        'timeable': '2|1-2',
-        'week': '2-18|2',
-        'location': '一号教学楼101'
-      }, {
-        'className': '数据库概论',
-        'timeable': '3|1-2',
-        'week': '1-18|1',
-        'location': '一号教学楼101'
-      }, {
-        'className': '形势政策',
-        'timeable': '4|1-2',
-        'week': '5-18|2',
-        'location': '一号教学楼101'
-      }, {
-        'className': '计算机网络',
-        'timeable': '5|1-2',
-        'week': '1-9|8',
-        'location': '一号教学楼101'
-      }
-    ],
-
-
+    color: ["ffcccc", '15a892', 'e6ccff', 'cce6ff', 'ffbf80','80bfff'],
+    currentCourse:[{}]
   },
+  
+  //测试数据
+  getTerm: {
+    year: 2018,
+    week: 8
+  },
+  getSche: [
+    {
+      'className': '高等数学',
+      'time': [1, 1, 3],// 数据格式：星期,第几节,第几节课
+      'week': [1, 18, 1],// 数据格式：所有单双周0 1 2,第几周,第几周
+      'location': '一号教学楼101'
+    }, {
+      'className': '大学英语',
+      'time': [2, 1, 2],
+      'week': [2, 2, 18],
+      'location': '一号教学楼101'
+    }, {
+      'className': '大学英语2',
+      'time': [2, 3, 4],
+      'week': [2, 2, 18],
+      'location': '一号教学楼101'
+    }, {
+      'className': '大学英语2',
+      'time': [2, 5, 7],
+      'week': [2, 2, 18],
+      'location': '一号教学楼101'
+    }, {
+      'className': '数据库概论',
+      'time': [3, 4, 5],
+      'week': [1, 1, 18],
+      'location': '一号教学楼101'
+    }, {
+      'className': '形势政策',
+      'time': [4, 5, 6],
+      'week': [2, 5, 18],
+      'location': '一号教学楼101'
+    }, {
+      'className': '计算机网络',
+      'time': [5, 1, 2],
+      'week': [0, 1, 9],
+      'location': '一号教学楼101'
+    }
+  ],
+
+
+
   changeWeek: function () {
 
+  },
+
+  setCurrentSche: function (getSche) {
+    var that = this
+    that.data.currentCourse = new Array
+
+    var i = 0;
+    while ("undefined" != typeof getSche[i]) {
+      var id = (getSche[i].time[0] ) * 10 + getSche[i].time[1] //表示课程id
+      
+      that.data.currentCourse[id]={
+        'className': getSche[i].className,
+        'time': getSche[i].time,
+        'lengthTime': getSche[i].time[2] - getSche[i].time[1] + 1,
+        'week': getSche[i].week,
+        'location': getSche[i].location
+      }
+      i++;
+    }
+    this.setData({
+      currentCourse: that.data.currentCourse
+    })
+
+    console.log(this.data.currentCourse)
   },
 
   /**
@@ -64,7 +102,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.setCurrentSche(this.getSche)
   },
 
   /**
