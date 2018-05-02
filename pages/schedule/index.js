71283e8c336682267e3ea7247dbd98453a98cb1c
Fanioc <fanioc@163.com>
2018-05-02 15:44:11 +0800

    课表增加背景模块(已注释，未显示..)

--- pages/schedule/index.js
+++ pages/schedule/index.js
@@ -130,7 +130,9 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.getSche.splice(2,1)
+    this.setCurrentSche(this.getSche)
+    wx.stopPullDownRefresh();
   },
   /**
    * 页面上拉触底事件的处理函数
