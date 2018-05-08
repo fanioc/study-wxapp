ef52f0d3b4987a8d9e291cf569d5b33ca19d8dbe
卢林杨 <281203035@qq.com>
2018-05-08 22:21:24 +0800

    增加动态圈模块的部分功能

--- pages/community/index.js
+++ pages/community/index.js
@@ -7,14 +7,15 @@ Page({
   data: {
     currentTab:0,//当前顶部切换组件的序号
     bannerList: [{ imgSrc:"debug0.png"}],
-    on:true
+    on:true,
+    feed: [],
+    feed_length: 0
   },
 //-----------------本页面自定义函数
-    /**
-     * 点击上tab切换，已调试
-     */
-  swichNav:function(e){
-console.log(e.currentTarget.dataset.current);
+    //-----swichNav
+  swichNav: function (e)//* 点击上tab切换，已调试
+  {
+//console.log(e.currentTarget.dataset.current);
 var that = this;
 
 if (this.data.currentTab === e.target.dataset.current) {
@@ -25,10 +26,39 @@ if (this.data.currentTab === e.target.dataset.current) {
   })
 }
   },
-      /**
-     * 获取动态圈板块的置顶广告的图片src，并存入数组bannerList:[]供wx for渲染
-     */
-  getBannerImgSrc:function(e)
+  //-----bindItemTap
+  bindItemTap: function ()//点击单动态模块时的动作
+   {
+    wx.navigateTo({
+      url: '../answer/answer'
+    })
+  },
+  getFeed: function ()//从服务器获取动态模块所需数据，设置data：feed, feed_length
+  {
+    var getFeed,feed_Array=[];
+    //----调试代码----
+    getFeed=[
+      {
+        feed_source_img: '/image/debug.png',
+        feed_source_name: 'debug',
+        feed_source_identity: 'debug',
+        question_title: 'debug',
+        question_describe: 'debug',
+        sort: 'debug',
+        comment_num: 999,
+        question_status: '已解决',
+
+      }
+    ];
+    //----调试代码----
+
+    //addtionRegion 后台数据
+    feed_Array=this.data.feed.concat(getFeed);
+    this.setData({ feed: feed_Array, feed_length:feed_Array.length});
+  },
+//----getBannerImgSrc
+
+  getBannerImgSrc: function (e)//获取动态圈板块的置顶广告的图片src，并存入数组bannerList:[]供wx for渲染
   {
    //addtionRegion
   },
@@ -37,7 +67,8 @@ if (this.data.currentTab === e.target.dataset.current) {
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+     this.getFeed();
+     this.getFeed();
   },
 
   /**
