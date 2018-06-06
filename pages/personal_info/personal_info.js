c0c945f7e6851080166966845ccf9544f47c3a0a
Fanioc <fanioc@163.com>
2018-06-06 15:14:41 +0800

    页面完善，userInfo 自定义组件跳转个人信息页面

--- pages/personal_info/personal_info.js
+++ pages/personal_info/personal_info.js
@@ -1,66 +1,89 @@
-// pages/personal_info/personal_info.js
+const CONSTANT = getApp().globalData.CONSTANT
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    uid: 0,
+    userInfo: {
+
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    console.log(options.uid)
+    this.getUserInfo(options.uid)
+  },
+
+  getUserInfo(uid) {
+    var that = this
+    this.setData({ uid: uid })
+    wx.request({
+      url: CONSTANT.API.getUserBasicInfo,
+      data: {
+        session: wx.getStorageSync('session'),
+        other_uid: uid
+      },
+      success: function (res) {
+        console.log(res)
+        if (res.data.errCode == 0)
+          that.setData({
+            userInfo: res.data.data
+          })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
