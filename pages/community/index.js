b2ff872ebbc24417c7a323646086319b1e4c0feb
Fanioc <fanioc@163.com>
2018-06-14 03:36:33 +0800

    完成大部分重构

--- pages/community/index.js
+++ pages/community/index.js
@@ -5,109 +5,179 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {
-    dynamics_refresh: 0,
-    currentTab: 0, //当前顶部切换组件的序号
-    bannerList: [{
-      imgSrc: "debug0.png"
-    }],
-    on: true,
-    feed: [],
-    feed_length: 0
+	data: {
+		feed: [], // 简化的定义方式,整个模块数据来源，用于wx::for渲染
+		feed_length: '', //feed数组长度，getFeed()会自动设置
+		hiddenToast: false, //控制提示刷新成功的toast
+		toastContent: '', //toast 提示内容
+		data_success: false, //只有当网络请求成功时才会设置为真然后对其渲染
+		//---------
+		tag_group_array: [{
+			content: '全部',
+			choose: true
+		},
+		{
+			content: '高等数学',
+			choose: false
+		},
+		{
+			content: 'C语言',
+			choose: false
+		},
+		], //debug
 
-  },
-  //-----------------本页面自定义函数
-  //-----swichNav
-  swichNav: function (e) //* 点击上tab切换，已调试
-  {
-    //console.log(e.currentTarget.dataset.current);
-    var that = this;
+	},
+	//-----------------本页面自定义函数
 
-    if (this.data.currentTab === e.target.dataset.current) {
-      return false;
-    } else {
-      that.setData({
-        currentTab: e.target.dataset.current
-      })
-    }
-  },
+	getFeed: function (mode = 1) //从服务器获取动态模块所需数据,参数mode表示设置feed时：1为追加 0为重写，设置data：feed, feed_length
+	{
+		var that = this;
+		var last_id = that.data.feed_length;
+		if (mode == 0) {
+			last_id = '';
+		}
 
+		//---
+		wx.showLoading({
+			title: '加载动态中',
+		})
+		//---
+		core.APIrequest('getDynamicList', {
+			last_id: last_id
+		}).then((result) => {
+			var getFeed, feed_Array = [];
+			getFeed = result;
+			console.log(getFeed)
+			if (mode) feed_Array = that.data.feed.concat(getFeed); //追加 else feed_Array = getFeed; //重置
+			else feed_Array = getFeed
+			if (typeof (feed_Array.length) == 'undefined') {
+				core.com.show_mToast('网络错误');
+			}
+			that.setData({
+				data_success: true,
+				feed: feed_Array,
+				feed_length: feed_Array[feed_Array.length - 1].question_id
+			});
+			wx.hideLoading();
+		}).catch((err) => {
+			core.APIerrCode(err, 2)
+			wx.hideLoading();
+		});
+	},
 
-  //----getBannerImgSrc
+	//--------------bindtap事件函数
 
-  getBannerImgSrc: function (e) //获取动态圈板块的置顶广告的图片src，并存入数组bannerList:[]供wx for渲染
-  {
-    //addtionRegion
-  },
-  add_dynamic: function () //编辑动态
-  {
-    wx.navigateTo({
-      url: "/pages/community/add_dynamic/add_dynamic"
-    });
-  },
-  //-----------------本页面自定义函数
+	nav_dynamic_page: function (e) { //转跳至动态详情页面,
+		var _THAT = this;
+		console.log('问题的index:', e.currentTarget.dataset.feedindex);
+		var feedIndex = e.currentTarget.dataset.feedindex;
+		//将当前问题问题传递给动态页面，
+
+		var question_id = _THAT.data.feed[feedIndex].question_id;
+		//转跳至动态详情页面
+
+		wx.navigateTo({
+			url: "/pages/community/dynamic/dynamic?question_id=" + question_id
+		});
+		//debugRegion
+	},
+
+	add_dynamic: function (e) {
+		wx.navigateTo({
+			url: '/pages/community/add_dynamic/add_dynamic',
+
+		})
+	},
+	set_tag: function (e) {
+		var tag_group = this.data.tag_group_array;
+
+		//addtionRegion
+		if (tag_group[0].choose) {
+			if (e.currentTarget.dataset.index != 0) {
+				tag_group[0].choose = false;
+				tag_group[e.currentTarget.dataset.index].choose = !tag_group[e.currentTarget.dataset.index].choose;
+			} else {
+				return true;
+			}
+
+		} else {
+			if (e.currentTarget.dataset.index != 0) {
+				tag_group[e.currentTarget.dataset.index].choose = !tag_group[e.currentTarget.dataset.index].choose;
+			} else {
+				for (let i in tag_group) {
+					tag_group[i].choose = false;
+				}
+				tag_group[0].choose = true;
+			}
+		}
+		this.setData({
+			tag_group_array: tag_group
+		});
+	},
+	//-----------------本页面自定义函数
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {},
+	onLoad: function (options) {
+		this.getFeed(0);
+	},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+	onReady: function () {
 
-  },
+	},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+	onShow: function () {
 
-  },
+	},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+	onHide: function () {
 
-  },
+	},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+	onUnload: function () {
 
-  },
+	},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+	onPullDownRefresh: function () {
+		this.getFeed(0);
+		wx.stopPullDownRefresh();
+		core.com.show_mToast('刷新成功');
 
-    if (this.data.currentTab == 0) //当currentTab为0时下拉刷新提示相应模块刷新数据
-    {
-      this.setData({
-        dynamics_refresh: !this.data.dynamics_refresh
-      });
-      console.log(this.data.dynamics_refresh)
-    } else {
-
-    }
-    wx.stopPullDownRefresh();
-  },
+	},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+	onReachBottom: function () {
+		//console.log(getCurrentPages()[0].is, _API.get_dynamic_array);
+		if (this.data.feed_length == 1) {
+			core.com.show_mToast('没有更多了')
+			return;
+		} else {
+			this.getFeed(1);
+		}
+	},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+	onShareAppMessage: function () {
 
-  }
+	}
 })
