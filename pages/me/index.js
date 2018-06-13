4b9e443a62b929da455c8a3d77b0c656dc996585
Fanioc <fanioc@163.com>
2018-06-14 05:04:23 +0800

    优化社区

--- pages/me/index.js
+++ pages/me/index.js
@@ -34,7 +34,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({ userInfo: getApp().globalData.me})
+    this.setData({ userInfo: core.userInfo[0]})
   },
 
   /**
